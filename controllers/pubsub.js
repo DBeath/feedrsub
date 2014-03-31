5d96cb7275e5bbc160c365ebfb4fa4c675ab29aa
David Beath <davidgbeath@gmail.com>
2014-03-31 16:13:25 +1300

    Added pubsub subscription

--- controllers/pubsub.js
+++ controllers/pubsub.js
@@ -1,7 +1,6 @@
 var config = require('../config.json');
 var mongo = require('../models/mongodb.js');
 var request = require('request');
-var urllib = require('url');
 var crypto = require('crypto');
 
 module.exports.pubsubController = function (options) {
@@ -19,14 +18,25 @@ function pubsub (options) {
       'pass': options.password,
       'sendImmediately': true
     }
-  };   
+  };
+
+  this.pending = {};   
 };
 
 pubsub.prototype.verification = function (req, res) {
-  var params = urllib.parse(req.url, true, true);
   var data;
 
-  if(!params.query['hub.topic'] || !params.query['hub.mode'])
+  if ( !req.query['hub.topic'] || !req.query['hub.mode'] ) {
+    return this.sendError(req, res, 400, 'Bad Request');
+  };
+
+  switch ( req.query['hub.mode'] ) {
+    case 'denied':
+      res.send(200, req.query['hub.challenge']);
+    case 'subscribe':
+    case 'unsubscribe':
+      res.send(200, req.query['hub.challenge']);
+  }
 };
 
 pubsub.prototype.notification = function (req, res) {
@@ -35,6 +45,49 @@ pubsub.prototype.notification = function (req, res) {
 
 pubsub.prototype.subscribe = function (req, res) {
 
+};
+
+pubsub.prototype.setSubscription = function (mode, topic, hub, callback) {
+  var uniqueCallbackUrl = callbackurl + 
+    (callbackurl.replacereplace(/^https?:\/\//i, "").match(/\//)?"":"/") +
+    (callbackurl.match(/\?/)?"&":"?") +
+    'topic=' + encodeURIComponent(topic) + 
+    '&hub=' + encodeURIComponent(hub);
+
+  var form = {
+    'hub.callback': uniqueCallbackUrl;
+    'hub.mode': mode,
+    'hub.topic': topic,
+    'hub.verify': 'sync'
+  };
+
+  if (this.secret) {
+    form['hub.secret'] = crypto.createHmac("sha1", this.secret).update(topic).digest("hex");
+  };
+
+  if (this.format === 'json' || this.format === 'JSON') {
+    form['format'] = 'json';
+  };
+
+  var postParams = {
+    url: hub,
+    form: form,
+    encoding: 'utf-8'
+  };
+
+  if (this.auth) {
+    postParams.auth = this.auth;
+  };
+
+  request.post(postParams, function (err, res, body) {
+    if (err) console.log(err);
+
+    if (res.statusCode === 202) {
+      callback(null, 'Accepted');
+    } else {
+      console.log('%s Denied', res.statusCode);
+    };
+  })
 };
 
 pubsub.prototype.unsubscribe = function (req, res) {
