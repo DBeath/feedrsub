b6f907bf2212f27ccda166e176f363790c1845d0
David Beath <davidgbeath@gmail.com>
2014-05-28 16:52:06 +1200

    Updated configuration and 404

--- config/index.js
+++ config/index.js
@@ -6,8 +6,10 @@ var env = nconf.get("NODE_ENV") || 'development';
 nconf.file(env, './config/' + env + '.json');
 nconf.file('default', './config/default.json');
 
-console.log('NODE_ENV: '+ nconf.get('NODE_ENV'));
-console.log('Database: '+ nconf.get('express:connstring'));
 var conf = nconf.load();
-console.log(conf.express.connstring);
+
+console.log('NODE_ENV: '+ env);
+console.log('Database: '+ conf.express.connstring);
+console.log();
+
 module.exports = conf;
