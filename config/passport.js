e8e3cc2383b594467f1077c7c8f15af00e0382de
David Beath <davidgbeath@gmail.com>
2014-07-29 16:59:42 +1200

    Updated user to control login attempts

--- config/passport.js
+++ config/passport.js
@@ -7,6 +7,8 @@ var validator = require('validator');
 
 var User = require('../models/user');
 
+var loginFailureMessage = 'Email or Password is incorrect.';
+
 passport.serializeUser(function (user, done) {
   return done(null, user.id);
 });
@@ -54,27 +56,47 @@ passport.use('local-login', new LocalStrategy({
 },
 function (req, email, password, done) {
   process.nextTick(function () {
-    User.findOne({email: email }, function (err, user) {
-      if (err) return done(err);
-      if (!user) {
-        console.log('No user found');
-        return done(null, false, req.flash('loginMessage', 'No user found.'));
-      };
-      user.comparePassword(password, function (err, isMatch) {
-        if (err) return done(err);
-        if (!isMatch) {
-          console.log('Wrong password');
-          return done(null, false, req.flash('loginMessage', 'Wrong password.'));
-        };
-        return done(null, user);
-      });
-      // if (!user.validPassword(password)) {
-      //   console.log('Wrong password');
-      //   return done(null, false, req.flash('loginMessage', 'Wrong password.'));
-      // };
+    // User.findOne({email: email }, function (err, user) {
+    //   if (err) return done(err);
+    //   if (!user) {
+    //     console.log('No user found');
+    //     return done(null, false, req.flash('loginMessage', 'No user found.'));
+    //   };
+    //   user.comparePassword(password, function (err, isMatch) {
+    //     if (err) return done(err);
+    //     if (!isMatch) {
+    //       console.log('Wrong password');
+    //       return done(null, false, req.flash('loginMessage', 'Wrong password.'));
+    //     };
+    //     return done(null, user);
+    //   });
+    //   // if (!user.validPassword(password)) {
+    //   //   console.log('Wrong password');
+    //   //   return done(null, false, req.flash('loginMessage', 'Wrong password.'));
+    //   // };
       
-      // return done(null, user);
-    }); 
+    //   // return done(null, user);
+    // }); 
+
+    User.getAuthenticated(email, password, function (err, user, reason) {
+      if (err) return done(err);
+      if (user) {
+        console.log('Login success for ' + email);
+        return done(null, user);
+      };
+      var reasons = User.failedLogin;
+      switch (reason) {
+        case reasons.NOT_FOUND:
+        case reasons.PASSWORD_INCORRECT:
+          console.log('Login failed for ' + email);
+          return done(null, false, req.flash('loginMessage', loginFailureMessage));
+          break;
+        case reasons.MAX_ATTEMPTS:
+          console.log('Account locked for ' + email);
+          return done(null, false, req.flash('loginMessage', 'Too many login attempts.'));
+          break;
+      };
+    });
   });
 }));
 
